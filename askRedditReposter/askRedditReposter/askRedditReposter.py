8d1c00c4d18861786c79ffb0f7b3d4b7902fcf12
hyperwing <drdavidwing@gmail.com>
2018-07-30 01:47:05 -0500

    reddit instance updated

--- askRedditReposter/askRedditReposter/askRedditReposter.py
+++ askRedditReposter/askRedditReposter/askRedditReposter.py
@@ -1,15 +1,16 @@
 import praw
 
 
-client_id = 'kHoe'
+client_id = 'GzZ2Zw0xuuTXww    '
 client_secret = 'QGwoiJVH1OH92xUrSaP7w9cycKM'
 password = 'P@ssw0rd96'
 username = 'pyeng'
+user_agent = 'kHoe'
 
 
 reddit = praw.Reddit(client_id=client_id,
                      client_secret = client_secret, password = password,
-                     user_agent='USERAGENT', username = username)
+                     user_agent=user_agent, username = username)
 
 for subreddit in reddit.subreddits.default(limit=None):
     print(subreddit)
